e55afd4282cfc36fa90fc73b02d371d87ea26182
LucianoMariotti <luciano.mariotti99@gmail.com>
2024-02-22 23:33:32 -0300

    FIX: Footer styles

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -18,22 +18,7 @@ export default function Footer({ containerRef }: FooterProps) {
 
   return (
     <div className="px-5 h-28 md:h-52 flex flex-col justify-end">
-      {/* <div className="flex justify-between pb-5 text-sm md:text-lg font-medium items-center">
-        <p className="">Luciano Mariotti</p>
-        <div className="flex gap-5 hidden md:flex gap-3 justify-center text-xs">
-          <Link href='#about'>Sobre mi</Link>
-                <Link href='#contact'>Experiencia</Link>
-                <Link href='/proyectos'>Proyectos</Link>
-          Hecho con ♥
-        </div>
-        <button
-          onClick={scrollToTop}
-          className="group p-5 bg-light rounded-full hover:bg-dark cursor-pointer duration-500 "
-        >
-          <ArrowUp className="group-hover:stroke-white  " />
-        </button>
-      </div> */}
-      <div className="grid grid-cols-3 items-center">
+      <div className="grid grid-cols-2 md:grid-cols-3 items-center">
         <p className=" text-sm md:text-lg font-medium">Luciano Mariotti</p>
         <div className="flex hidden md:flex gap-3 justify-center text-xs">
           Hecho con ♥
