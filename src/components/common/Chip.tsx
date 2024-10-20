e257096882c8b1d1b274d90296ad8168d4450040
mariodev99 <lucianomariodev@gmail.com>
2024-10-20 20:55:53 +0200

    fix component

--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -1,7 +1,7 @@
-export default function Chip({ children }) {
-  return (
-    <span className="text-sm md:text-base py-1 px-2 md:px-4 font-regular border rounded-full">
-      {children}
-    </span>
-  );
-}
+// export default function Chip({ children }) {
+//   return (
+//     <span className="text-sm md:text-base py-1 px-2 md:px-4 font-regular border rounded-full">
+//       {children}
+//     </span>
+//   );
+// }
